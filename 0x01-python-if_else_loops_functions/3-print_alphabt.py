300ce1f43087f95b0fad74fef0672f190fad68f8
Okene4000 <okeneblessing@gmail.com>
2023-03-08 20:25:24 +0000

    alphabet2

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for character in range(97, 123):
     if chr(alphabet) != 'e' and chr(alphabet) != 'q':
-        print("{}". format(chr(alphabet)), end="")
+        print("{}".format(chr(alphabet)), end="")
