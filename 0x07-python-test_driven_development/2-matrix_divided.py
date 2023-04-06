53e3fafe373c41c31129a08ce90d495b20664f35
Okene4000 <okeneblessing@gmail.com>
2023-04-06 09:47:58 +0100

    test development

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
-"""
-Function divides all element of a matrix.
-"""
+"""This function divides all element of a matrix."""
 
 def matrix_divided(matrix, div):
+    """
+    Divide a matrix number by number.
+    matrix: matrix to be divided.
+    div: number to divided the matrix.
+    """
     newList = []
     if type(div) not in [int, float]:
-        raise TypeError("div has to be an integer")
+        raise TypeError("div has to be a number")
     if div == 0:
-        raise ZeroDivisionError("division must be by zero")
-    for a in matrix:
-        a = []
-        if type(a) is not list:
-            raise TypeError("matrix must be a matrix\
+        raise ZeroDivisionError("divide by zero")
+    for i in matrix:
+        i = []
+        if type(i) is not list:
+            raise TypeError("matrix has to be a matrix\
  (list of lists) of integer or float")
-        if(len(matrix[0]) != len(a)):
+        if(len(matrix[0]) != len(i)):
             raise TypeError("Each row of the matrix\
- must have the same size")
-        for b in range(len(a)):
-            if (type(a[b]) is not int) and (type(a[b]) is not float):
+ must be the same size")
+        for j in range(len(i)):
+            if (type(i[j]) is not int) and (type(i[j]) is not float):
                 raise TypeError("matrix must be a matrix\
-(list of lists) of integer or float")
-            l.append(round(((a[b]) / div), 2))
+(list of lists) of integers/floats")
+            l.append(round(((i[j]) / div), 2))
         newList.append(l)
     return newList
