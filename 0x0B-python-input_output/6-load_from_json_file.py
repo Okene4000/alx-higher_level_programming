6e5861e43947c70b1be504f15965181d9f3de00d
Okene4000 <okeneblessing@gmail.com>
2023-04-13 20:37:51 +0100

    input and output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines JSON file-reading function."""
+"""Defines a JSON file-reading function."""
 import json
 
 
 def load_from_json_file(filename):
-    """Create Python object from a JSON file."""
+    """Create a Python object from a JSON file."""
     with open(filename) as f:
-        return json.load(f)i
+        return json.load(f)
